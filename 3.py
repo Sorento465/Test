709d72d3d05e2299daf2f14d7ec118bb4e346c35
Azamat <163117346+Sorento465@users.noreply.github.com>
2024-04-04 09:22:46 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,4 +2,8 @@ n = int(input())
 for i in range(n):
     first = [int(x) for x in input().split()]
     second = [int(x) for x in input().split()]
-
+    fmin = min(first)
+    fmax = max(first)
+    print(fmin, fmax)
+    answer = [num for num in second if fmin <= num <= fmax]
+    print(" ".join(str(x) for x in answer))
